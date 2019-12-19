24bcd31cdd6d368cb3a6a72b5d9cb8342a933bfb
Thibaut Guénédal <thibautguenedal@gmail.com>
2019-12-19 23:49:42 +0000

    bg-BG

--- src/stores/locale.js
+++ src/stores/locale.js
@@ -14,13 +14,13 @@ export const localeFlag = {
   "br": "br",
   "lv": "lv",
   "pl": "pl",
-  "bg": "bg",
+  "bg-BG": "bg",
 }
 export const localeTranslators = {
   "hu": ["Lemax#7166"],
   "lv": ["Syrius#8114"],
   "pl": ["Lament"],
-  "bg": ["Silence#5339"],
+  "bg-BG": ["Silence#5339"],
 }
 
 for(let cc of Object.keys(languages).filter(s => s != "en")) {
