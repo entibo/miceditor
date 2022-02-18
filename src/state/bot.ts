04290119b63201d1dcff1db776f1f4100222651a
Thibaut Guénédal <thibautguenedal@gmail.com>
2022-02-18 13:13:08 +0100

    Update bot module

--- src/state/bot.ts
+++ src/state/bot.ts
@@ -128,14 +128,13 @@ export const botLuaModule = `local botName = "${botName}"
 local lastXML = nil
 local loadMapWhenBotLeaves = false
 
--- Feel free to customize this module.
-
 function eventNewGame()
     tfm.exec.disableAutoShaman(true)
     tfm.exec.disableAutoNewGame(true)
     tfm.exec.disableAutoTimeLeft(true)
     tfm.exec.disableAfkDeath(true)
 end
+eventNewGame()
 
 function numPlayers()
     local count = 0
@@ -163,6 +162,7 @@ function eventNewPlayer(name)
     if name == botName then
         addPopup()
     else
+        bindMouseAndKeyboard(name)
         tfm.exec.respawnPlayer(name)
     end
 end
@@ -180,4 +180,89 @@ function eventChatCommand(name, cmd)
 end
 system.disableChatCommandDisplay("reload")
 system.disableChatCommandDisplay("You")
+
+local spawnPoint = {}
+function eventPlayerRespawn(name)
+    if spawnPoint[name] then
+        tfm.exec.movePlayer(name, spawnPoint[name].x, spawnPoint[name].y)
+        return
+    end
+end
+
+function bindMouseAndKeyboard(name)
+    system.bindMouse(name, true)
+    tfm.exec.bindKeyboard(name, 16, true, true)
+    tfm.exec.bindKeyboard(name, 32, true, true)
+    tfm.exec.bindKeyboard(name, 77, true, true)
+    tfm.exec.bindKeyboard(name, 67, true, true)
+    tfm.exec.bindKeyboard(name, 0, true, true)
+    tfm.exec.bindKeyboard(name, 2, true, true)
+end
+for name, player in pairs(tfm.get.room.playerList) do
+    bindMouseAndKeyboard(name)
+end
+
+local lastClick = {}
+function eventMouse(name, x, y)
+    local time = os.time()
+    if not lastClick[name] then lastClick[name] = {time = 0} end
+    if time - lastClick[name].time < 500 then
+        spawnPoint[name] = {
+            x = lastClick[name].x and lastClick[name].x or x,
+            y = lastClick[name].y and lastClick[name].y or y
+        }
+        lastClick[name] = {time = 0}
+        tfm.exec.killPlayer(name)
+        return
+    end
+    lastClick[name] = {time = time, x = x, y = y}
+    tfm.exec.movePlayer(name, x, y)
+end
+
+local cooldowns = {speed = 0}
+local facing = {}
+function eventKeyboard(name, keyCode, down, x, y)
+    local time = os.time()
+    if keyCode == 0 or keyCode == 2 then
+        facing[name] = keyCode == 2
+        return
+    end
+    if keyCode == 16 then
+        if time - cooldowns.speed < 1000 then return end
+        cooldowns.speed = time
+        tfm.exec.movePlayer(name, 0, 0, true, facing[name] and 60 or -60, 0,
+                            true)
+        return
+    end
+    if keyCode == 32 then
+        tfm.exec.movePlayer(name, 0, 0, true, 0, -50, false)
+    end
+    if keyCode == 77 then tfm.exec.killPlayer(name) end
+    if keyCode == 67 then
+        spawnPoint[name] = {
+            x = tfm.get.room.playerList[name].x,
+            y = tfm.get.room.playerList[name].y
+        }
+        tfm.exec.killPlayer(name)
+    end
+end
+
+-- Feel free to customize this module
+-- This is the minimum code required to make the bot work
+--[[
+  local botName = "Entibot#5692"
+  system.disableChatCommandDisplay("You", true)
+
+  function eventNewPlayer(name)
+      if name == botName then
+          ui.addPopup(5692, 2, "xml", botName)
+      end
+  end
+
+  function eventPopupAnswer(id, name, str)
+      if id == 5692 and name == botName then
+          tfm.exec.newGame(str)
+      end
+  end
+]]
 `
