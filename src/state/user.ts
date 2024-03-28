ec8dd52a98a68f91b54a6f643dad9231e8810bfa
entibo <thibautguenedal@gmail.com>
2024-03-28 11:31:39 +0100

    build

--- src/state/user.ts
+++ src/state/user.ts
@@ -76,6 +76,6 @@ export const colorPalette = persistentWritable("colorPalette", [
 ])
 
 
-export const currentVersion = "2.13.0"
+export const currentVersion = "2.13.1"
 export const previousVersion = localStorage.getItem("version")
 localStorage.setItem("version", currentVersion)
